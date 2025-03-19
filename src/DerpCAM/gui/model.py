f823c2601d3206eb6edbb85bbdc63072f42bc914
Krzysztof Foltman <wdev@foltman.com>
2025-03-19 15:54:47 +0000

    Fix another regression in text colouring.

--- src/DerpCAM/gui/model.py
+++ src/DerpCAM/gui/model.py
@@ -784,7 +784,7 @@ class OperationTreeItem(CAMTreeItem):
     def toTextColor(self, property):
         if property is self.prop_doc:
             pda = PresetDerivedAttributes(self)
-            if self.cutter.max_doc and pda.doc > self.cutter.max_doc:
+            if self.cutter.max_doc and pda.doc and pda.doc > self.cutter.max_doc:
                 return "red"
             return None
         return None
